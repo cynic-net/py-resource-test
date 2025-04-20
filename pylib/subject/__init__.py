652a0264901a669a13d73fd9b5ade15f4f95b018
Curt J. Sampson <cjs@cynic.net>
2025-04-20 05:21:18 +0000

    subject: Add functions demonstrating "sugared" API

--- pylib/subject/__init__.py
+++ pylib/subject/__init__.py
@@ -52,10 +52,11 @@ module_loaded = True
 
 #   Instead of importlib.resources, which has lots of annoying variations
 #   from 3.7 through 3.12, we use the backported version from PyPI.
-from importlib_resources  import files as resfiles
+from    importlib_resources  import files as resfiles
+import  importlib_resources as resources
 
-def contents_textfile():
-    ''' Read the ``textfile`` resource.
+def read_textfile_core():
+    ''' Use the core API to read the ``textfile`` resource.
         1. The no-argument version of `files()` is not available before
            Python 3.12; we must install the PyPI ``importlib-resources``\
            package to get it in Python ≤ 3.11.
@@ -64,3 +65,10 @@ def contents_textfile():
            to use `files()` since it's about as simple anyway.
     '''
     return resfiles().joinpath('textfile').read_text()
+
+def read_textfile_sugar_modreq():
+    ''' Demo that old API can't take `None`, an `Anchor` is required. '''
+    return resources.read_text(None, 'textfile')
+
+def read_textfile_sugar_selfmodule():
+    return resources.read_text(__name__, 'textfile')
